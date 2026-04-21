fd0abad71c98dfbbe5cc831eb5ff163bc0523015
Oleksandra Maryanko <maryankoaleksandra@gmail.com>
2026-04-21 13:46:24 +0200

    style: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import KpiCards from "@/components/ui/KpiCard";
-import "./globals.css";
 import Sections from "@/components/ui/SectionCard";
 
 export default function Home() {
